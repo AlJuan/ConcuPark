f2120289448db0311eeed413910b822082865a97
kevinmlew <kevin.martin.lew@gmail.com>
2016-04-02 19:07:24 -0300

    Fix

--- src/Concupark.cpp
+++ src/Concupark.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <unistd.h>
 
 #include "MemoriaCompartida.h"
 #include "model/Juego.h"
@@ -26,22 +27,24 @@ int main() {
 		cout << "ERROR memoria compartida" << endl;
 	}
 
-	Juego juego1;
-	Juego juego2;
-	juego1.setCosto(3);
-	juego2.setCosto(5);
+	Juego juego;
+	juego.setCosto(1);
+	memoria.escribir(juego, 0);
 
-	memoria.escribir(juego1, 0);
-	memoria.escribir(juego2, 1);
+	pid_t procId = fork ();
 
-	Juego j1 = memoria.leer(0);
-	Juego j2 = memoria.leer(1);
+	if ( procId == 0 ) {
+		juego.setCosto(2);
+		sleep(2);
+		memoria.escribir(juego, 0);
+	} else {
+		while (true){
+			Juego j = memoria.leer(0);
+			cout << j.getCosto() << endl;
+			sleep(3);
+		}
 
-	if (j1.getCosto() == juego1.getCosto())
-		cout << "igual" << endl;
-	if (j2.getCosto() == juego2.getCosto())
-		cout << "igual" << endl;
-
+	}
 
 
 	//ConfiguracionParque confParque = Parser.parse();
