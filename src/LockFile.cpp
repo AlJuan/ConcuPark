95c9dd82e3edf85aa12b1a6f4cd3a125dca563e2
kevinmlew <kevin.martin.lew@gmail.com>
2016-04-30 19:57:17 -0300

    Fix lock

--- src/LockFile.cpp
+++ src/LockFile.cpp
@@ -6,7 +6,7 @@ LockFile :: LockFile ( const std::string nombre ) {
 	this->fl.l_type = F_WRLCK;
 	this->fl.l_whence = SEEK_SET;
 	this->fl.l_start = 0;
-	this->fl.l_len = 0;
+	this->fl.l_len = sizeof(char);
 	this->fd = open ( this->nombre.c_str(),O_CREAT|O_WRONLY,0777 );
 }
 
